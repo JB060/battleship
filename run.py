89464f81633d312f9d57ada2bdcb6defcc9a9bee
Jason Byrne <jasonbyrne060@gmail.com>
2024-05-27 18:26:55 +0000

    defined the rules for the game and built th board six=ze

--- run.py
+++ run.py
@@ -1,3 +1,48 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+game_board -[]
+
+player_one = {
+    "name": "player 1",
+    "wins": 0,
+}
+
+player_two = {
+    "name": "player 2"
+    "wins" : 0,
+}
+
+colors - {
+    "reset":"\033[00m",
+    "red":"\033[91m",
+    "blue":"\033[94m",
+    "cyan":"\033[96m",
+}
+
+#Building our 6X6 Board
+def build_game_board(board):
+    for item in range(6):
+        board.append(["0" * 6])
+
+def show_board(board):
+    for row in board:
+        print("".join (row))
+
+def load_game(board):
+    print("WELCOME TO BATTLESHIP")
+    print("Find & sink the other players ships")
+    print("THIS IS A 2 PLAYER GAME ")
+    del board[:]
+    build_game_board(board):
+    print(colors['blue'])
+    show_board(board)
+    print(colors['reset'])
+    ship_col = randint(1, len(board))
+    ship_row = randint(1, len(board[0]))
+    return{
+        
+    }
+
+
+
+
